2a67cabfea5dca8fb65017a36e8b44d9ba1e9f03
DanielM-IT <7ebe7@gmail.com>
2019-07-28 18:59:26 +1200

    Functional game mechanics added

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,28 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour
 {
     private Rigidbody2D myRigidBody;
     private Animator myAnim;
     public float bunnyJumpForce = 500f;
+    private float bunnyHurtTime = -1;
+    private Collider2D myCollider;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Jump"))
+        if (bunnyHurtTime == -1)
         {
+            if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
+            {
+                if (myRigidBody.velocity.y < 0)
+                {
+                    myRigidBody.velocity = Vector2.zero;
+                }
+                if (jumpsLeft == 1)
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJumpForce /** 0.75f*/);
+                }
+                else
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJumpForce);
+                }
+                
+                jumpsLeft--;
+            }
+
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if (Time.time > bunnyHurtTime + 2)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
+            {
+                moveLefter.enabled = false;
+            }
+
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+            bunnyHurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+            myRigidBody.velocity = Vector2.zero;
             myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            myCollider.enabled = false;
+        }
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsLeft = 2;
         }
 
-        myAnim.SetFloat("vVelocity", /*Mathf.Abs(*/myRigidBody.velocity.y)/*)*/;
     }
 }
---
+++ Assets/Scripts/PrefabSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    private float nextSpawn = 0;
+    public Transform prefabToSpawn;
+    public AnimationCurve spawnCurve;
+    public float curveLengthInSeconds = 30f;
+    private float startTime;
+    public float jitter = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+
+            float curvePosition = (Time.time - startTime) / curveLengthInSeconds;
+            if (curvePosition > 1f)
+            {
+                curvePosition = 1f;
+                startTime = Time.time;
+            }
+
+            nextSpawn = Time.time + spawnCurve.Evaluate(curvePosition) + Random.Range(-jitter, jitter);
+        }
+    }
+}
